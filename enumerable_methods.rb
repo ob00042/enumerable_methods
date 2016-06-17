80214c8d42b61ac93a45442645612bd935910b32
ob00042 <ob00042@gmail.com>
2016-06-17 19:31:27 +0300

    my_each_with_index working

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -13,8 +13,21 @@ module Enumerable
 
 	end
 
+	def my_each_with_index
+
+		i=0
+
+		while i<self.size
+			yield(self[i],i)
+			i+=1
+		end
+		self
+
+	end
+
 
 end
 
-([2,4,3,1]).my_each{|x| puts x*2}
+#([2,4,3,1]).my_each{|x| puts x*2}
+([2,4,3,1]).my_each_with_index{|x,y| puts "#{x},#{y}"}
 
